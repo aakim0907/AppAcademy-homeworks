6d123941ba589033d1dfeb637409891c8b1208f4
Young Eun Kim <aakim0907@gmail.com>
2017-06-24 21:56:00 -0700

    Create controllers

---
+++ w5d1/MyRails/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @users = User.all
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to links_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
--- w5d1/MyRails/config/routes.rb
+++ w5d1/MyRails/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'links', to: 'links#index'
+  # get 'links/:id', to: 'links#show'
+  # post 'links', to: 'links#create'
+  # patch 'links/:id', to: 'links#update'
+  # put 'links/:id', to: 'links#update'
+  # delete 'links/:id', to: 'links#destroy'
+
+  resources :users, only: [:new, :create]
+  resources :links
+  resources :comments, only: [:create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end
