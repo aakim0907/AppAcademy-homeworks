8bec5f7d41cca6dcdd65e65bf9c31d3d6f744906
Young Eun Kim <aakim0907@gmail.com>
2017-07-01 18:26:09 -0700

    Complete phase 2

--- w6d1/js_review/w5d4_intro/phase_2_enumerable.js
+++ w6d1/js_review/w5d4_intro/phase_2_enumerable.js
@@ -1,6 +1,4 @@
 // Array#myEach(callback)
-// - receives a callback function and executes the callback for each element in the array
-// Note that JavaScript's forEach function has no return value (returns undefined)
 Array.prototype.myEach = function (cb) {
   for (var i = 0; i < this.length; i++) {
     cb(this[i]);
@@ -8,19 +6,41 @@ Array.prototype.myEach = function (cb) {
 };
 
 console.log("Array#myEach");
-console.log([1,2,3].myEach((el) => console.log(el)));
+console.log([1,2,3].myEach(el => console.log(el)));
 
 // Array#myMap(callback) - receives a callback function, returns a new array of the results of calling the callback function on each element of the array
 // should use myEach and a closure
-// Array#myReduce(callback, [initialValue]) - (like Ruby's Array#inject) receives a callback function, and optional initial value, returns an accumulator by applying the callback function to each element and the result of the last invocation of the callback (or initial value if supplied)
-// initialValue is optional and should default to the first element of the array if not provided
+Array.prototype.myMap = function (cb) {
+  const mapped = [];
+  this.myEach(el => mapped.push(cb(el)));
+  return mapped;
+};
+
+console.log("Array#myMap");
+console.log([1,2,3].myMap(el => el * 2));
+
+// Array#myReduce(callback, [initialValue])
+Array.prototype.myReduce = function (cb, initialValue) {
+  const arr = this;
+
+  if (!initialValue) {
+    initialValue = this[0];
+    arr = arr.slice(1);
+  }
+
+  let result = initialValue;
+
+  arr.myEach(el => result = cb(result, el));
+
+  return result;
+};
 
 // // without initialValue
-// [1, 2, 3].myReduce(function(acc, el) {
-//   return acc + el;
-// }); // => 6
+[1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}); // => 6
 //
 // // with initialValue
-// [1, 2, 3].myReduce(function(acc, el) {
-//   return acc + el;
-// }, 25); // => 31
+[1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}, 25); // => 31
