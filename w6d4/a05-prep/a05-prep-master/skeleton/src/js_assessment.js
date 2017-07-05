1fd128dd7a9c75d41a31277b72d738f667eb6bfa
Young Eun Kim <aakim0907@gmail.com>
2017-07-05 15:52:46 -0700

    Upload w6d4 homework

---
+++ w6d4/a05-prep/a05-prep-master/skeleton/src/js_assessment.js
@@ -0,0 +1,36 @@
+// write String.prototype.mySlice. It should take a start index and an
+// (optional) end index.
+String.prototype.mySlice = function (startIdx, endIdx) {
+  let sliced = "";
+
+  if (!endIdx) {
+    endIdx = this.length;
+  }
+
+  for (let i = startIdx; i < endIdx && i < this.length; i++) {
+    sliced += this[i];
+  }
+
+  return sliced;
+};
+
+// write Array.prototype.myReduce (analogous to Ruby's Array#inject).
+
+// write Array.prototype.quickSort(comparator). Here's a quick refresher if
+// you've forgotten how quickSort works:
+//   - choose a pivot element from the array (usually the first)
+//   - for each remaining element of the array:
+//     - if the element is less than the pivot, put it in the left half of the
+//     array.
+//     - otherwise, put it in the right half of the array.
+//   - recursively call quickSort on the left and right halves, and return the
+//   full sorted array.
+
+// write myFind(array, callback). It should return the first element for which
+// callback returns true, or undefined if none is found.
+
+// write sumNPrimes(n)
+
+// write Function.prototype.myBind.
+
+// write Function.prototype.inherits.
