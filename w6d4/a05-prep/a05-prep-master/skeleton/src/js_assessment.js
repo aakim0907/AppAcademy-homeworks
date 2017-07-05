6390816e6ed57d117e7fe15673ecb9f9b0c822b7
Young Eun Kim <aakim0907@gmail.com>
2017-07-05 16:36:30 -0700

    Delete solution & solve a05-prep

--- w6d4/a05-prep/a05-prep-master/skeleton/src/js_assessment.js
+++ w6d4/a05-prep/a05-prep-master/skeleton/src/js_assessment.js
@@ -1,13 +1,12 @@
 // write String.prototype.mySlice. It should take a start index and an
 // (optional) end index.
-String.prototype.mySlice = function (startIdx, endIdx) {
+String.prototype.mySlice = function (start, end) {
   let sliced = "";
-
-  if (!endIdx) {
-    endIdx = this.length;
+  if (typeof end === "undefined") {
+    end = this.length;
   }
 
-  for (let i = startIdx; i < endIdx && i < this.length; i++) {
+  for (let i = start; i < end && i < this.length; i++) {
     sliced += this[i];
   }
 
@@ -15,6 +14,12 @@ String.prototype.mySlice = function (startIdx, endIdx) {
 };
 
 // write Array.prototype.myReduce (analogous to Ruby's Array#inject).
+Array.prototype.myReduce = function (callback, acc = this.shift()) {
+  for (let i = 0; i < this.length; i++) {
+    acc = callback(acc, this[i]);
+  }
+  return acc;
+};
 
 // write Array.prototype.quickSort(comparator). Here's a quick refresher if
 // you've forgotten how quickSort works:
@@ -25,12 +30,88 @@ String.prototype.mySlice = function (startIdx, endIdx) {
 //     - otherwise, put it in the right half of the array.
 //   - recursively call quickSort on the left and right halves, and return the
 //   full sorted array.
+Array.prototype.quickSort = function (comparator) {
+  if (this.length <= 1) return this;
+
+  if (typeof comparator !== "function") {
+    comparator = function(x, y) {
+      if (x < y) {
+        return -1;
+      } else if (x === y) {
+        return 0;
+      } else {
+        return 1;
+      }
+    };
+  }
+
+  const pivot = this[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < this.length; i++) {
+    if (comparator(this[i], pivot) <= 0) {
+      left.push(this[i]);
+    } else {
+      right.push(this[i]);
+    }
+  }
+
+  return left.quickSort(comparator)
+      .concat([pivot])
+      .concat(right.quickSort(comparator));
+};
 
 // write myFind(array, callback). It should return the first element for which
 // callback returns true, or undefined if none is found.
+function myFind(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array[i];
+    }
+  }
+}
 
 // write sumNPrimes(n)
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  } else {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function sumNPrimes(n) {
+  let sumPrimes = 0;
+  let primeCount = 0;
+  let num = 1;
+
+  while (primeCount < n) {
+    if (isPrime(num)) {
+      sumPrimes += num;
+      primeCount++;
+    }
+    num++;
+  }
+
+  return sumPrimes;
+}
 
 // write Function.prototype.myBind.
+Function.prototype.myBind = function (ctx, ...bindArgs) {
+  return (...callArgs) => this.apply(ctx, bindArgs.concat(callArgs));
+};
 
 // write Function.prototype.inherits.
+Function.prototype.inherits = function (parent) {
+  // this.prototype = Object.create(parent.prototype);
+  function Surrogate() {}
+  Surrogate.prototype = parent.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+};
