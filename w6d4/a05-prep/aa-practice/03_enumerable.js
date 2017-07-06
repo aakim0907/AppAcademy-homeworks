f3956dce94c648098a7b5e0ff8b4eaa2321a6093
Young Eun Kim <aakim0907@gmail.com>
2017-07-05 17:04:00 -0700

    Final organization

--- w6d4/a05-prep/aa-practice/03_enumerable.js
+++
@@ -1,5 +0,0 @@
-// Monkey patch the Array class and add a my_inject method. If my_inject receives
-// no argument, then use the first element of the array as the default accumulator.
-
-
-// Write a version of my each that calls a proc on each key, value pair
