51fdaae85d83900efc09c3e52fe8083c38b9aa5d
Young Eun Kim <aakim0907@gmail.com>
2017-07-06 22:30:55 -0700

    Add input logic

--- w6d5/calculator.jsx
+++ w6d5/calculator.jsx
@@ -3,15 +3,28 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // your code here
+    this.state = {result: 0, num1: "", num2: ""};
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
   }
 
-  // your code here
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1> 
+        <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={this.state.num1}/>
+        <input onChange={this.setNum2} value={this.state.num2}/>
       </div>
     );
   }
